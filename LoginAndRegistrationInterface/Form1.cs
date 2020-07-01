916d5307c3ffe05784beb8d2228fae7c9acba885
1609661842@qq.com <1609661842@qq.com>
2020-07-01 18:59:04 +0800

    蛇，食物修改

--- LoginAndRegistrationInterface/Form1.cs
+++ LoginAndRegistrationInterface/Form1.cs
@@ -132,6 +132,8 @@ namespace LoginAndRegistrationInterface
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
+
+
             // Application.Exit();
             Close();
         }
